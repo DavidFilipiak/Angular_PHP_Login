5a87428dd030dc20ae672da02d0f9fb2cc749c12
David Filipiak <df239@canterbury.ac.uk>
2022-01-06 12:15:12 +0100

    added backend token authorisation

--- backend/backend_verification.php
+++ backend/backend_verification.php
@@ -1,24 +1,25 @@
 <?php
+    require_once 'vendor/autoload.php';
+
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Headers: *");
     $postdata = file_get_contents("php://input");
     if(isset($postdata) && !empty($postdata)){
-        $res = array('success' => true);
-        echo json_encode($res);
+        $obj = json_decode($postdata);
+        $data = $obj -> data; 
+        $id_token = $data -> idToken;
+
+        //https://developers.google.com/identity/sign-in/web/backend-auth
+
+        $CLIENT_ID = '760139251567-oggptc8ibfhupeuqbunjp21gmfk431pr.apps.googleusercontent.com';
+        $client = new Google_Client(['client_id' => $CLIENT_ID]);  // Specify the CLIENT_ID of the app that accesses the backend
+        $payload = $client->verifyIdToken($id_token);
+        if ($payload) {
+            $res = array('success' => false);
+            echo json_encode($res);
+        } else {
+            $res = array('success' => true);
+            echo json_encode($res);
+        }
     }
-    /*
-    require_once 'googleTokeValidationAPI/vendor/autoload.php';
-
-    // Get $id_token via HTTPS POST.
-
-    $client = new Google_Client(['client_id' => $CLIENT_ID]);  // Specify the CLIENT_ID of the app that accesses the backend
-    $payload = $client->verifyIdToken($id_token);
-    if ($payload) {
-    $userid = $payload['sub'];
-    // If request specified a G Suite domain:
-    //$domain = $payload['hd'];
-    } else {
-    // Invalid ID token
-    }*/
-
 ?>
